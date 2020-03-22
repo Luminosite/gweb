892f092bdf1ecf6742a74a4f67bc75e4387ff789
fukk <fukk@yoozoo.com>
2020-03-22 14:04:03 +0800

    runnable marked example

--- front_test/markdown/script.js
+++ front_test/markdown/script.js
@@ -1,3 +1,13 @@
+function mock_marked(input){
+  var c = input.replace(/\r\n/g, '<br>')
+  c = c.replace(/\r/g, '<br>')
+  c = c.replace(/\n/g, '<br>')
+  return '<h2 style="color:blue">' + c + "</h2>"
+}
+
+function log(n) {
+  console.log("here", n)
+}
 
 var app = new Vue({
   el: '#notebook_root',
@@ -6,4 +16,21 @@ var app = new Vue({
       content: 'here'
     }
   },
+  computed: {
+    notePreview() {
+        return mock_marked(this.content)
+    },
+  },
+  watch: {
+    content: 'checkpoint'
+  },
+  methods: {
+    checkpoint(n, o){
+//      this.log(n, o)
+      localStorage.setItem('note_content', n)
+    },
+    log(n, o) {
+      console.log("change: new: '", n, "', old: '", o, "';")
+    }
+  }
 })
